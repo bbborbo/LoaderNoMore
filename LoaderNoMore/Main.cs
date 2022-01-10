5f2c28a83b68f50c1bc8503a01d3dc92e5375024
BORBO <bwofodol@gmail.com>
2022-01-09 17:32:43 -0700

    moved skill nuking to a hook that is guaranteed to happen after all skill slots have stopped being modified

--- LoaderNoMore/Main.cs
+++ LoaderNoMore/Main.cs
@@ -34,17 +34,141 @@ namespace LoaderNoMore
 
             On.RoR2.SurvivorCatalog.SetSurvivorDefs += NukeLoader;
             //On.RoR2.BodyCatalog.GetBodyPrefabSkillSlots += NukeIonSurge;
-            NukeSurge();
+            On.RoR2.Loadout.GenerateViewables += NukeIonSurge;
+            //NukeSurge();
         }
 
-        private GenericSkill[] NukeIonSurge(On.RoR2.BodyCatalog.orig_GetBodyPrefabSkillSlots orig, BodyIndex bodyIndex)
+        private void NukeIonSurge(On.RoR2.Loadout.orig_GenerateViewables orig)
         {
             string fullSkillString = SkillsToRemove.Value;
             fullSkillString = fullSkillString.Replace(" ", "");
             fullSkillString = fullSkillString.Replace("-", " ");
             string[] skillNames = fullSkillString.Split(',');
+            foreach (string nameToken in skillNames)
+            {
+                //Debug.Log(nameToken);
+            }
+            foreach (string nameToken in skillNames)
+            {
+                string[] substrings = nameToken.Split('_');
+                foreach (string substring in substrings)
+                {
+                    //Debug.Log(substring);
+                }
 
-            return orig(bodyIndex);
+                LoaderNoMoreSkillSlotData currentSlotData = new LoaderNoMoreSkillSlotData();
+                SkillFamily skillFamily = null;
+                foreach (LoaderNoMoreSkillSlotData skillSlotData in allSkillSlotData)
+                {
+                    SkillFamily family = skillSlotData.skillFamily;
+                    if (skillSlotData.bodyName == substrings[0] && family.name == substrings[1])
+                    {
+                        currentSlotData = skillSlotData;
+                        skillFamily = family;
+                    }
+                }
+
+                if (skillFamily == null)
+                {
+                    GameObject body = Resources.Load<GameObject>("prefabs/characterbodies/" + substrings[0]);//BodyCatalog.FindBodyPrefab(substrings[0]);
+                    if (body != null)
+                    {
+                        SkillLocator skillLocator = body.GetComponent<SkillLocator>();
+                        if (skillLocator != null)
+                        {
+                            GenericSkill skillSlot = null;
+                            bool doPassive = false;
+
+                            switch (substrings[1].ToLower())
+                            {
+                                case "passive":
+                                    doPassive = true;
+                                    break;
+                                case "primary":
+                                    skillSlot = skillLocator.primary;
+                                    break;
+                                case "secondary":
+                                    skillSlot = skillLocator.secondary;
+                                    break;
+                                case "utility":
+                                    skillSlot = skillLocator.utility;
+                                    break;
+                                case "special":
+                                    skillSlot = skillLocator.special;
+                                    break;
+                            }
+
+                            if (doPassive)
+                            {
+                                SkillLocator.PassiveSkill passiveSkill = skillLocator.passiveSkill;
+                                passiveSkill.enabled = false;
+                            }
+                            else if (skillSlot != null)
+                            {
+                                skillFamily = skillSlot.skillFamily;
+                                currentSlotData.indices = new List<string>();
+                                currentSlotData.skillFamily = skillFamily;
+                                allSkillSlotData.Add(currentSlotData);
+                            }
+                        }
+                    }
+                }
+
+                if (skillFamily != null)
+                {
+                    SkillFamily.Variant[] variants = skillFamily.variants;
+                    string index = substrings[2];
+                    Debug.Log(index);
+
+                    currentSlotData.indices.Add(index);
+                }
+            }
+
+
+            foreach (LoaderNoMoreSkillSlotData skillSlotData in allSkillSlotData)
+            {
+                SkillFamily skillFamily = skillSlotData.skillFamily;
+                if (skillFamily != null)
+                {
+                    List<SkillFamily.Variant> variants = new List<SkillFamily.Variant>(skillFamily.variants);
+
+                    int advance = 0;
+                    int c = variants.Count;
+                    for (int i = 0; i < c; i++)
+                    {
+                        Debug.Log("G");
+                        bool match = false;
+                        foreach (string index in skillSlotData.indices)
+                        {
+                            if (index == i.ToString())
+                            {
+                                match = true;
+                                //Debug.Log($"Match found for {nameToken} in the survivor catalog! Removing!");
+                            }
+                        }
+
+                        if (match)//skip this entry
+                        {
+                            variants.RemoveAt(i - advance);
+                            advance++;
+                        }
+                        else if (advance > 0)//back up every other entry for each one skipped
+                        {
+
+                        }
+                    }
+
+                    if (advance > 0)
+                    {
+                        skillFamily.variants = variants.ToArray();
+                    }
+                }
+                else
+                {
+                    Debug.Log("saghfbaehgbrfhjrqwaef");
+                }
+            }
+            orig();
         }
 
         internal static List<LoaderNoMoreSkillSlotData> allSkillSlotData = new List<LoaderNoMoreSkillSlotData>(0);
